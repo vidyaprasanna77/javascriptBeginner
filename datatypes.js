290942b841bd3b723aa145bf7dfa61d2ddb6ef8c
vidyaprasanna77 <100849455+vidyaprasanna77@users.noreply.github.com>
2022-04-10 09:21:05 +0530

    variables and datatypes

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+let price;
+let quantity;//variable declaration
+let total;
+price = 5; //assigning value
+quantity = 14;
+total = price * quantity;// total cost
+ let e1 = document.getElementById("cost");
+ e1.textContent= '$' + total ; 
